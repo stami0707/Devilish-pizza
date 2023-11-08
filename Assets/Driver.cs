65c68a821bd42c5d32a60be804ad5c8a7b990a2e
Süli Tamara <stami0707@gmail.com>
2023-11-08 14:51:41 +0100

    Basic objects and control
    Created driver behavior and control by user

---
+++ Assets/Driver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 75f;
+    [SerializeField] float moveSpeed = 4f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
